76412ff9f6d8c74c28e142254cf11108753c736d
ItzDFPlayer <deniskovalevskiy91@gmail.com>
2024-08-02 16:00:46 +0300

    Add skip detach image as a system prop
    Due to this change in EvoX
    https://github.com/Evolution-XYZ/frameworks_base/commit/553e826562dd1792061b377a77cf12f590c44934

--- config.mk
+++ config.mk
@@ -9,7 +9,7 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/bin,$(TARGET_COPY_OUT_VENDOR)/bin) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/etc,$(TARGET_COPY_OUT_VENDOR)/etc) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/lib,$(TARGET_COPY_OUT_VENDOR)/lib) \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64) \
+	$(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/camera,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/camera)
 
 PRODUCT_PACKAGES += \
@@ -23,11 +23,12 @@ PRODUCT_PACKAGES += \
 
 # Props
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.google.lens.oem_camera_package=com.android.camera
+	ro.com.google.lens.oem_camera_package=com.android.camera
 
 PRODUCT_SYSTEM_PROPERTIES += \
-    persist.vendor.camera.privapp.list=com.android.camera 
-
+	persist.vendor.camera.privapp.list=com.android.camera \
+	persist.sys.cam.skip_detach_image=true
+	
 # Sepolicy
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     vendor/xiaomi/miuicamera/sepolicy/private
